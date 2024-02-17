3715d125be520559ed4f0d2d3d06ef566261e875
Shj286 <159392351+Shj286@users.noreply.github.com>
2024-02-17 12:08:29 -0800

    created button component

---
+++ acresweb/src/components/TaskButton.js
@@ -0,0 +1,8 @@
+import Button from '@mui/material/Button';
+import * as React from 'react';
+
+export default function TaskButton() {
+    return (
+        <Button variant="outlined">~Yes we work here</Button>
+    );
+  }
