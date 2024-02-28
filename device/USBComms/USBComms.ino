aba82a99ec3c7c8a800244f99e7d64dc3d4479da
ConnorCross1 <159590397+ConnorCross1@users.noreply.github.com>
2024-02-28 15:18:02 -0800

    Update USBComms.ino

--- device/USBComms/USBComms.ino
+++ device/USBComms/USBComms.ino
@@ -2,7 +2,6 @@
 Same as default flicker program but flicker speed is controlled by USB input.
 */
 
-int n = 1;//light flicker speed multiplier
 int n;//light flicker speed multiplier
 
 void setup() {
@@ -12,17 +11,10 @@ void setup() {
 }
 
 void loop() {
-  if(Serial.available()){//checks if there is data to be read
-    n = (int)Serial.read();
-  }
   if(Serial.available()>0){//checks if there is data to be read
     n = Serial.read();
     Serial.println("debug");
 
-  digitalWrite(LED_BUILTIN, LOW);//default speed is 1000ms between flickers
-  delay(1000/n);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000/n);
     Serial.println(n);
   }
 }
