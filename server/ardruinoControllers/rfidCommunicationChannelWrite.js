45fbed4b30038b165f8a3b440beb6571f1907ca2
ConnorCross1 <159590397+ConnorCross1@users.noreply.github.com>
2024-03-02 18:42:23 -0800

    Writes to Adruino

---
+++ server/ardruinoControllers/rfidCommunicationChannelWrite.js
@@ -0,0 +1,23 @@
+const { SerialPort } = require('serialport')
+
+
+// Create a port
+const port = new SerialPort({
+  path: "COM3",
+  baudRate: 9600,
+})
+// Open errors will be emitted as an error event
+port.on('error', function(err) {
+    console.log('Error: ', err.message)
+  })
+
+port.on("open", function() {
+    console.log("-- Connection opened --");
+    
+         port.write('Hello world', function(err) {
+             if (err) {
+             return console.log('Error on write: ', err.message)
+             }
+             console.log('message written')
+         })
+});
